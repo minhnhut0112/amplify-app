eb1cfec0c84e1aabd2c487378d29cf798dba40d9
minhnhut1712 <nminhnhut0112@gmail.com>
2024-12-06 09:11:49 +0700

    create crud function

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,96 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Todo.create({ content: newTodoCentent });
   }
+
+  const [isEdit, setIsEdit] = useState<number | null>();
+  const [todoCentent, setTodoContent] = useState<string>("");
+  const [newTodoCentent, setNewTodoContent] = useState<string>("");
+
+  const updateTodo = async (id: string, content: string) => {
+    await client.models.Todo.update({ id, content });
+  };
+
+  const deleteTodo = async (id: string) => {
+    await client.models.Todo.delete({ id });
+  };
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>My todos app</h1>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id}>
+            <div className="item-container">
+              {isEdit !== Number(todo.id) ? (
+                <>
+                  <label htmlFor={String(todo.id)}>{todo.content}</label>
+                  <div className="button-container">
+                    <button
+                      className="btn-edit"
+                      onClick={() => {
+                        setIsEdit(Number(todo.id));
+                        setTodoContent(String(todo.content));
+                      }}
+                    >
+                      edit
+                    </button>
+                    <button
+                      onClick={() => deleteTodo(todo.id)}
+                      className="btn-delete"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <form
+                  className="change-container"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    updateTodo(todo.id, todoCentent);
+                  }}
+                >
+                  <input
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setTodoContent(e.target.value)
+                    }
+                    className="todo-input"
+                    type="text"
+                    value={todoCentent}
+                  />
+                  <button
+                    onClick={() => {
+                      setIsEdit(null);
+                      setTodoContent("");
+                    }}
+                    className="btn-close"
+                  >
+                    Close
+                  </button>
+                  <button className="btn-save">Save</button>
+                </form>
+              )}
+            </div>
+          </li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+
+      <hr />
+
+      <form className="todo-form">
+        <input
+          value={newTodoCentent}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setNewTodoContent(e.target.value)
+          }
+          id="todoName"
+          type="text"
+          placeholder="Add a todo"
+        />
+        <button onClick={createTodo}>Add a todo</button>
+      </form>
     </main>
   );
 }
